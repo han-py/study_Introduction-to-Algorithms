c7b388f86f856203489f56eabc50d78b80c51a6d
HaPpY <930333105@qq.com>
2025-11-26 15:29:38 +0800

    feat(build): 添加本地调试宏定义支持
    - 在 CMakeLists.txt 中添加 ACM_LOCAL 宏定义
    - 在 main.cpp 中添加文件重定向代码块
    - 支持从 data.in 读取输入并输出到 data.out
    - 使用宏定义控制本地调试功能的开关

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+#ifdef ACM_LOCAL
+    freopen("data.in", "r", stdin);
+    freopen("data.out", "w", stdout);
+#endif
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
