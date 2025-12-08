8bea91e7e68d75dfc82ce1a5900c86093c04e765
HaPpY <930333105@qq.com>
2025-12-08 16:15:06 +0800

    refactor(tree): 优化红黑树实现与输出格式
    - 调整节点信息打印格式，增加缩进以提高可读性
    - 重构树结构打印逻辑，增强视觉层次感
    - 统一旋转操作中的提示信息为英文，提升一致性
    - 简化修复过程中的描述文字，去除冗余说明
    - 修改插入节点时的分隔线样式，统一界面风格
    - 更新主程序入口的标题显示方式，改善用户体验
    - 优化空值节点的处理及显示逻辑

--- C3/U13/RED-BLACK-TREE/RedBlackTree.cpp
+++ C3/U13/RED-BLACK-TREE/RedBlackTree.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <queue>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
 // 红黑树节点颜色枚举
@@ -27,12 +30,12 @@ private:
     // 打印节点信息的辅助函数
     void printNodeInfo(Node *node, const string &description) {
         if (node == nullptr) {
-            cout << description << ": nullptr" << endl;
+            cout << "  " << description << ": nullptr" << endl;
             return;
         }
         
-        cout << description << ": " << node->data 
-             << "(" << (node->color == RED ? "R" : "B") << ")";
+        cout << "  " << description << ": " << node->data 
+             << " (" << (node->color == RED ? "R" : "B") << ")";
         if (node->parent) {
             cout << ", Parent: " << node->parent->data;
         } else {
@@ -41,37 +44,9 @@ private:
         cout << endl;
     }
     
-    // 打印树结构的辅助函数
-    void printTreeStructureHelper(Node *node, string indent, bool last) {
-        if (node != nullptr) {
-            cout << indent;
-            if (last) {
-                cout << "+-";
-                indent += "  ";
-            } else {
-                cout << "|-";
-                indent += "| ";
-            }
-            
-            cout << node->data << "(" << (node->color == RED ? "R" : "B") << ")" << endl;
-            
-            if (node->left != nullptr || node->right != nullptr) {
-                if (node->left)
-                    printTreeStructureHelper(node->left, indent, node->right == nullptr);
-                else
-                    cout << indent << "+-nullptr(L)" << endl;
-                    
-                if (node->right)
-                    printTreeStructureHelper(node->right, indent, true);
-                else
-                    cout << indent << "+-nullptr(R)" << endl;
-            }
-        }
-    }
-    
     // 左旋操作
     void rotateLeft(Node *&node) {
-        cout << "执行左旋操作，以节点 " << node->data << " 为中心" << endl;
+        cout << "  Left rotate at node " << node->data << endl;
         
         Node *rightChild = node->right;
         node->right = rightChild->left;
@@ -90,13 +65,11 @@ private:
             
         rightChild->left = node;
         node->parent = rightChild;
-        
-        cout << "左旋完成" << endl;
     }
     
     // 右旋操作
     void rotateRight(Node *&node) {
-        cout << "执行右旋操作，以节点 " << node->data << " 为中心" << endl;
+        cout << "  Right rotate at node " << node->data << endl;
         
         Node *leftChild = node->left;
         node->left = leftChild->right;
@@ -115,13 +88,11 @@ private:
             
         leftChild->right = node;
         node->parent = leftChild;
-        
-        cout << "右旋完成" << endl;
     }
     
     // 修复插入后违反的红黑树性质
     void fixInsertViolation(Node *&node) {
-        cout << "\\n开始修复插入冲突，当前节点: " << node->data << endl;
+        cout << "\n开始修复红黑树性质，当前节点: " << node->data << endl;
         
         Node *parent = nullptr;
         Node *grandparent = nullptr;
@@ -132,41 +103,35 @@ private:
             parent = node->parent;
             grandparent = parent->parent;
             
-            printNodeInfo(parent, "父节点");
-            printNodeInfo(grandparent, "祖父节点");
+            printNodeInfo(parent, "Parent");
+            printNodeInfo(grandparent, "Grandparent");
             
             /*  Case : 当父节点是祖父节点的左孩子 */
             if (parent == grandparent->left) {
                 Node *uncle = grandparent->right;
-                printNodeInfo(uncle, "叔叔节点");
+                printNodeInfo(uncle, "Uncle");
                 
                 /* Case 1: 叔叔节点是红色，只需要重新着色 */
                 if (uncle != nullptr && uncle->color == RED) {
-                    cout << "情况1: 叔叔节点是红色，执行重新着色" << endl;
+                    cout << "  情况1: 叔叔节点是红色，重新着色" << endl;
                     grandparent->color = RED;
                     parent->color = BLACK;
                     uncle->color = BLACK;
-                    cout << "祖父节点 " << grandparent->data << " 设为红色" << endl;
-                    cout << "父节点 " << parent->data << " 设为黑色" << endl;
-                    cout << "叔叔节点 " << uncle->data << " 设为黑色" << endl;
                     node = grandparent;
                 }
                 
                 else {
                     /* Case 2: node是父节点的右孩子，需要左旋 */
                     if (node == parent->right) {
-                        cout << "情况2: 当前节点是父节点的右孩子，先对父节点执行左旋" << endl;
+                        cout << "  情况2: 当前节点是父节点的右孩子，对父节点左旋" << endl;
                         rotateLeft(parent);
                         node = parent;
                         parent = node->parent;
-                        printNodeInfo(node, "旋转后的新节点");
-                        printNodeInfo(parent, "旋转后的新父节点");
                     }
                     
                     /* Case 3: node是父节点的左孩子，需要右旋 */
-                    cout << "情况3: 对祖父节点执行右旋" << endl;
+                    cout << "  情况3: 对祖父节点右旋" << endl;
                     rotateRight(grandparent);
-                    cout << "交换父节点和祖父节点的颜色" << endl;
                     swap(parent->color, grandparent->color);
                     node = parent;
                 }
@@ -175,46 +140,40 @@ private:
             /* Case : 当父节点是祖父节点的右孩子 */
             else {
                 Node *uncle = grandparent->left;
-                printNodeInfo(uncle, "叔叔节点");
+                printNodeInfo(uncle, "Uncle");
                 
                 /* Case 1: 叔叔节点是红色，只需要重新着色 */
                 if ((uncle != nullptr) && (uncle->color == RED)) {
-                    cout << "情况1: 叔叔节点是红色，执行重新着色" << endl;
+                    cout << "  情况1: 叔叔节点是红色，重新着色" << endl;
                     grandparent->color = RED;
                     parent->color = BLACK;
                     uncle->color = BLACK;
-                    cout << "祖父节点 " << grandparent->data << " 设为红色" << endl;
-                    cout << "父节点 " << parent->data << " 设为黑色" << endl;
-                    cout << "叔叔节点 " << uncle->data << " 设为黑色" << endl;
                     node = grandparent;
                 }
                 
                 else {
                     /* Case 2: node是父节点的左孩子，需要右旋 */
                     if (node == parent->left) {
-                        cout << "情况2: 当前节点是父节点的左孩子，先对父节点执行右旋" << endl;
+                        cout << "  情况2: 当前节点是父节点的左孩子，对父节点右旋" << endl;
                         rotateRight(parent);
                         node = parent;
                         parent = node->parent;
-                        printNodeInfo(node, "旋转后的新节点");
-                        printNodeInfo(parent, "旋转后的新父节点");
                     }
                     
                     /* Case 3: node是父节点的右孩子，需要左旋 */
-                    cout << "情况3: 对祖父节点执行左旋" << endl;
+                    cout << "  情况3: 对祖父节点左旋" << endl;
                     rotateLeft(grandparent);
-                    cout << "交换父节点和祖父节点的颜色" << endl;
                     swap(parent->color, grandparent->color);
                     node = parent;
                 }
             }
             
-            cout << "当前修复步骤完成，进入下一轮检查\\n" << endl;
+            cout << "  完成本轮修复\n" << endl;
         }
         
-        cout << "将根节点设置为黑色" << endl;
+        cout << "设置根节点为黑色" << endl;
         root->color = BLACK;
-        cout << "插入冲突修复完成\\n" << endl;
+        cout << "红黑树性质修复完成\n" << endl;
     }
     
 public:
@@ -225,9 +184,9 @@ public:
     
     // 插入新节点
     void insert(const int &data) {
-        cout << "========================================" << endl;
+        cout << "----------------------------------------" << endl;
         cout << "插入节点: " << data << endl;
-        cout << "========================================" << endl;
+        cout << "----------------------------------------" << endl;
         
         Node *node = new Node(data);
         
@@ -239,7 +198,7 @@ public:
         
         cout << "最终树结构:" << endl;
         printTree();
-        cout << "\\n" << endl;
+        cout << "\n" << endl;
     }
     
     // BST插入的辅助函数
@@ -248,29 +207,28 @@ public:
         
         // 如果树为空，返回新节点
         if (root == nullptr) {
-            cout << "树为空，直接插入根节点" << endl;
+            cout << "  树为空，直接插入根节点" << endl;
             return node;
         }
             
         // 否则递归向下查找位置
         if (node->data < root->data) {
-            cout << "节点 " << node->data << " 小于 " << root->data << "，向左子树插入" << endl;
+            cout << "  节点 " << node->data << " 小于 " << root->data << "，向左子树插入" << endl;
             root->left = bstInsert(root->left, node);
             root->left->parent = root;
         }
         else if (node->data > root->data) {
-            cout << "节点 " << node->data << " 大于 " << root->data << "，向右子树插入" << endl;
+            cout << "  节点 " << node->data << " 大于 " << root->data << "，向右子树插入" << endl;
             root->right = bstInsert(root->right, node);
             root->right->parent = root;
         }
         else {
-            cout << "节点 " << node->data << " 已存在，不插入重复节点" << endl;
+            cout << "  节点 " << node->data << " 已存在，不插入重复节点" << endl;
             // 释放已分配的内存
             delete node;
             return root;
         }
         
-        cout << "BST插入完成" << endl;
         return root;
     }
     
@@ -317,15 +275,42 @@ public:
         cout << endl;
     }
     
-    // 打印树结构
+    // 打印树结构（使用标准的树形结构展示）
     void printTree() {
         if (root == nullptr) {
             cout << "空树" << endl;
             return;
         }
-        
-        cout << "树结构图:" << endl;
-        printTreeStructureHelper(root, "", true);
+
+        printTreeHelper(root, "", true);
+    }
+
+    // 树结构打印辅助函数
+    void printTreeHelper(Node *node, string indent, bool last) {
+        if (node != nullptr) {
+            cout << indent;
+            if (last) {
+                cout << "+-";
+                indent += "  ";
+            } else {
+                cout << "|-";
+                indent += "| ";
+            }
+
+            cout << node->data << "(" << (node->color == RED ? "R" : "B") << ")" << endl;
+
+            if (node->left != nullptr || node->right != nullptr) {
+                if (node->left)
+                    printTreeHelper(node->left, indent, node->right == nullptr);
+                else if (node->right)
+                    cout << indent << "+-nullptr" << endl;
+
+                if (node->right)
+                    printTreeHelper(node->right, indent, true);
+                else if (node->left)
+                    cout << indent << "+-nullptr" << endl;
+            }
+        }
     }
     
     // 搜索节点
@@ -358,8 +343,8 @@ int main() {
 #endif
     RedBlackTree tree;
     
-    cout << "红黑树示例程序\\n";
-    cout << "==============\\n\\n";
+    cout << "红黑树示例程序" << endl;
+    cout << "================" << endl << endl;
     
     // 插入一些测试数据
     int values[] = {10, 20, 30, 15, 25, 5, 1};
@@ -369,22 +354,22 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << values[i] << " ";
     }
-    cout << "\\n\\n";
+    cout << "\n\n";
     
     // 逐个插入并显示过程
     for (int i = 0; i < n; i++) {
         tree.insert(values[i]);
     }
     
-    cout << "==================================================" << endl;
+    cout << "========================================" << endl;
     cout << "所有插入操作完成" << endl;
-    cout << "==================================================\\n" << endl;
+    cout << "========================================\n" << endl;
     
     tree.inorder();
     tree.levelOrder();
     tree.printTree();
     
-    cout << "\\n搜索测试:\\n";
+    cout << "\n搜索测试:" << endl;
     tree.search(15);
     tree.search(100);
     
